218d0acaf79886cb84362eec6e4852a5f136ea43
Fernando-Moceri <fernando.moceri@titanclarity.com>
2023-10-10 19:03:44 -0300

    adding .env to the project

--- database/session_manager.py
+++ database/session_manager.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
-from database.config import engine
+from sqlalchemy import create_engine
 
+load_dotenv()
+
+engine = create_engine(os.getenv('DB_URL'))
 Session = sessionmaker(bind=engine)
 
 
